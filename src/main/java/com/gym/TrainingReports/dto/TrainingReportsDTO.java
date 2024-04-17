aa56404c727bb72dab15d305c289fbf70eb0decb
[jdanielcalle] <[jdanielcalle0@gmail.com]>
2024-04-16 21:37:27 -0500

    Updated files

--- src/main/java/com/gym/TrainingReports/dto/TrainingReportsDTO.java
+++ src/main/java/com/gym/TrainingReports/dto/TrainingReportsDTO.java
@@ -1,4 +1,20 @@
 package com.gym.TrainingReports.dto;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrainingReportsDTO {
+    private int idTrainee;
+    private String nameTrainee;
+    private int idTrainer;
+    private String nameTrainer;
+    private LocalDate trainingDate;
+    private String trainingType;
+    private String trainingDuration;
 }
--- src/main/java/com/gym/TrainingReports/model/TrainingReports.java
+++ src/main/java/com/gym/TrainingReports/model/TrainingReports.java
@@ -1,4 +1,21 @@
 package com.gym.TrainingReports.model;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrainingReports {
+    private int idReport;
+    private int idTrainee;
+    private String nameTrainee;
+    private int idTrainer;
+    private String nameTrainer;
+    private LocalDate trainingDate;
+    private String trainingType;
+    private String trainingDuration;
 }
